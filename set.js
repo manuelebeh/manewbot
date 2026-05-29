9dc0f8b45a6b7fc285bf3a37cd44953c65324bbc
Emmanuel Ebeh <emmanuelebehdev@gmail.com>
2026-05-29 10:54:24 +0000

    fix: update environment configuration to set mode to private and adjust sticker pack and bot name

--- set.js
+++ set.js
@@ -9,8 +9,8 @@ module.exports = {
   NOM_OWNER: process.env.NOM_OWNER || '',
   NUMERO_OWNER: process.env.NUMERO_OWNER || '',
   MODE: process.env.MODE || 'public',
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || 'ᴏᴠʟ-ᴍᴅ-ᴠ𝟸',
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || 'Manewbot',
   STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || '',
   DATABASE: process.env.DATABASE,
-  NOM_BOT: process.env.NOM_BOT || '🤖 OVL-MD BOT V2',
+  NOM_BOT: process.env.NOM_BOT || 'Manewbot',
 };
